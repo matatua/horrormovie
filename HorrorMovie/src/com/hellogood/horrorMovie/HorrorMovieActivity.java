a1b221e871a56053952a59a6f214a916b1cc77ef
renxh.cn@gmail.com <renxh.cn@gmail.com@e1746d0e-f09c-4a80-9ad9-74197a477825>
2012-02-17 17:44:26 +0000

    初始导入

---
+++ HorrorMovie/src/com/hellogood/horrorMovie/HorrorMovieActivity.java
@@ -0,0 +1,13 @@
+package com.hellogood.horrorMovie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HorrorMovieActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
